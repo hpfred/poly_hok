ebfb9eaa05d71d4f3b0cd0304fd951067ea5f888
hpfred <frepantunes@yahoo.com.br>
2026-05-24 16:10:38 -0300

    Testes ao saxpy cuda pra ver se está rodando certo

--- benchmarks/cuda/saxpy.cu
+++ benchmarks/cuda/saxpy.cu
@@ -34,8 +34,10 @@ int main(int argc, char const *argv[])
     // Filling a and b arrays
     for (int i = 0; i < size; i++)
     {
-        host_a[i] = i + 1;
-        host_b[i] = i + 1;
+        // host_a[i] = i + 1;
+        // host_b[i] = i + 1;
+        host_a[i] = 1;
+        host_b[i] = 1;
     }
 
     float *dev_a, *dev_b, *dev_result;
@@ -66,7 +68,13 @@ int main(int argc, char const *argv[])
     }
 
     cudaMemcpy(host_result, dev_result, size * sizeof(float), cudaMemcpyDeviceToHost);
-    
+    printf("Result: [");
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%f, ",host_result[i]);
+    }
+    printf("]\n");
+
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&time, start, stop);
