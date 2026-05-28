f02637b042c34589a555aaebca4beaf22700751f
hpfred <frepantunes@yahoo.com.br>
2026-05-27 22:11:07 -0300

    Voltou a otimização do SAXPY, para ter resultado skelib mais rapido que cuda

--- run_new2.sh
+++ run_new2.sh
@@ -8,11 +8,11 @@
 #   echo -n -e "$count.\t" 
 #   mix run benchmarks/ske_lib/nearest_neighbor.ex 900000000
 #done
-#echo "--------cuda saxpy------------------------"
-#for ((count=1;count<=30;count++)) do
-#   echo -n -e "$count.\t" 
-#   ./benchmarks/cuda/SAXPY 900000000
-#done
+echo "--------cuda saxpy------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   ./benchmarks/cuda/SAXPY 900000000
+done
 echo "--------ske_lib saxpy------------------------"
 for ((count=1;count<=30;count++)) do
    echo -n -e "$count.\t" 
