0a250c71972058c7f2b4435a90cf8ee69422079d
hpfred <frepantunes@yahoo.com.br>
2026-07-02 15:30:37 -0300

    Separados os EXLA e Pytorch, e script sh feito pra eles

---
+++ run_all_sequential.sh
@@ -0,0 +1,79 @@
+echo "--------default dot product------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   ./benchmarks/seq/dot_product.ex 200000000
+done
+echo "--------exla dot product------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/dot_product_EXLA.ex 200000000
+done
+echo "--------pytorch dot product------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/dot_product_pytorch.ex 200000000
+done
+
+echo "--------default nbodies------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   ./benchmarks/seq/nbodies.ex 5000
+done
+echo "--------exla nbodies------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/nbodies_EXLA.ex 5000
+done
+echo "--------pytorch nbodies------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/nbodies_pytoch.ex 5000
+done
+
+echo "--------default raytracer------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   ./benchmarks/seq/raytracer.ex 2160
+done
+echo "--------exla raytracer------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/raytracer_EXLA.ex 2160
+done
+echo "--------pytorch raytracer------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/raytracer_pytorch.ex 2160
+done
+
+echo "--------default nearest neighbor------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   ./benchmarks/seq/nearest_neighbor.ex 100000000
+done
+echo "--------exla nearest neighbor------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/nearest_neighbor_EXLA.ex 100000000
+done
+echo "--------pytorch nearest neighbor------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/nearest_neighbor_pytorch.ex 100000000
+done
+
+echo "--------default saxpy------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   ./benchmarks/seq/saxpy.ex 200000000
+done
+echo "--------exla saxpy------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/saxpy_EXLA.ex 200000000
+done
+echo "--------pytorch saxpy------------------------"
+for ((count=1;count<=30;count++)) do
+   echo -n -e "$count.\t" 
+   mix run benchmarks/seq/saxpy_pytorch.ex 200000000
+done
